a14d3995751d094aa5f25d4dff15a41b4991a797
Matias Morales <matias.andres.mh@gmail.com>
2021-01-04 01:37:09 -0300

    ready tamplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
+import{FormsModule} from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './pages/heroes/heroes.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,7 @@
+import { ReturnStatement } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HeroeModel } from '../../models/heroe.model';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
+  heroe : HeroeModel = new HeroeModel();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(form: NgForm){
+
+    if(form.invalid){
+
+console.log('formulario no valido');
+      return;
+    }
+
+    console.log(form);
+    console.log(this.heroe);
+
   }
 
 }
